ad3066e0149396907c8138fe2b3216dfe0df1e6d
ASDRAs <asdra030522@gmail.com>
2022-06-21 13:53:26 +0900

    cm1 undesigned

--- djangoProject/mysite/views.py
+++ djangoProject/mysite/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post
+from django.utils import timezone
+from .forms import PostCreateForm
 
-# Create your views here.
+def posts(request):
+    posts = Post.objects.filter(published_at__isnull=False).order_by('-published_at')
+    return render(request, 'posts.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'post_detail.html', {'post': post})
+
+def post_create(request):
+    if request.method == "POST":
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_at = timezone.now()
+            post.save()
+            return redirect('post_detail', id=post.id)
+    else:
+        form = PostCreateForm()
+    return render(request, 'post_create.html', {'form': form})
