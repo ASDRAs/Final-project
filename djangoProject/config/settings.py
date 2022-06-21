cb9d075108aa7d844ceb33b43393c445575d508d
ASDRAs <asdra030522@gmail.com>
2022-06-21 22:10:00 +0900

    cm5 ch

--- djangoProject/config/settings.py
+++ djangoProject/config/settings.py
@@ -71,7 +71,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
